d966fabe18566b94b710bf492783cb645232a6fd
Grant Warman <grant.warman@gmail.com>
2014-05-27 20:45:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Durhamchessclub::Application.config.secret_key_base = 'bdf43d87ed4ca00751f52579bd350ff1f9588e371a0f732b00bc3ce2ab60ca9a1d2e23dd7d48e1c86ed392198a5ffee08b929fe4440a2674c3d056ab80746ea1'
